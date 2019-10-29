de41524ab37936b14b42ace77ef2d38f3ee3c336
Oning-afk <949421402@qq.com>
2019-10-29 20:46:21 +0800

    轮播图修复

--- mall/mall-common/src/main/java/com/future/model/CommodityType.java
+++ mall/mall-common/src/main/java/com/future/model/CommodityType.java
@@ -4,12 +4,15 @@ import lombok.Data;
 import lombok.ToString;
 
 import javax.swing.*;
+import java.util.List;
+import java.util.Map;
 
 @Data
 @ToString
 public class CommodityType {
     private  Integer id;
     private  String name;
-    private  Integer pId;
+    private  Integer pid;
     private  Integer sorting;
+    private List<CommodityType> children;
 }
